2c2afbc37e1a2a497ee8b0b89ec715214695a7ab
Dominik Lakomicki <dominik.27@o2.pl>
2026-08-17 19:02:51 +0200

    //

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,13 +1,8 @@
 package core.basesyntax;
 
-public abstract class Figure {
+public abstract class Figure implements Area, Drawable {
     protected String color;
-
     public Figure(String color) {
         this.color = color;
     }
-
-    public abstract double getArea();
-
-    public abstract void draw();
 }
